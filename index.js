71203048d1ad7af867efe79c7359fb6823d45527
CompileYouth <CompileYouth@163.com>
2017-03-25 22:33:23 +0800

    Add script and style to page.

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import http from 'http';
 import app from './lib/app';
 
 const httpServer = http.createServer(app);
-httpServer.listen(8080, () => {
+httpServer.listen(3000, () => {
   console.log('Server is running at 8080...');
 });
