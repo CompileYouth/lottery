8da795baf2972a8fc48ca45c3f52a14e5fb3d288
CompileYouth <CompileYouth@163.com>
2017-03-25 11:58:17 +0800

    Add config.

---
+++ config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db: {
+
+  },
+  web: {
+    assets: {
+      urlPrefix: null
+    }
+  }
+};
---
+++ config/development.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db: {
+
+  },
+  web: {
+    assets: {
+      urlPrefix: 'http://127.0.0.1:8080'
+    }
+  }
+};
