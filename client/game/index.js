b67a35e59d816b5023758439924a003681b60f60
CompileYouth <CompileYouth@163.com>
2017-03-26 21:14:13 +0800

    Modify game js and style

--- client/game/index.js
+++ client/game/index.js
@@ -1,12 +1,22 @@
 $(document).ready(() => {
   const $gameBtn = $('#play-btn');
+  const interval = 10 * 1000; // 10s
 
   $gameBtn.on('click', () => {
+    
     $.ajax({
       url: '/lottery',
       method: 'POST'
     }).done((data) => {
-      
+      console.log(data);
     });
   });
+
+  function isLuckDog() {
+    if (!localStorage.getItem('prize')) {
+      return false;
+    }
+
+    return true;
+  }
 });
